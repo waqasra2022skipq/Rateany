60b87f8fa86ea15a1cd0be982c5ba4251b009909
Waqas Rahman <104453900+waqasra2022skipq@users.noreply.github.com>
2024-11-25 18:30:02 +0500

    h1 for SEO

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+    <header class="text-center my-4">
+        <h1>Find the top rated professionals and businesses</h1>
+    </header>
 
     @include('components.carousel', ['reviews' => $reviews])
     <div class="container my-5">
@@ -16,33 +19,12 @@
     </div>
 
 
-    <!-- Other Sections -->
 
-    {{-- @include('components.top-restaurants-card', ['topRestaurants' => $topRestaurants]) --}}
-
-    {{-- <div class="container my-5">
-        <h2 class="text-center mb-4">Gyms <i class="fa-solid fa-dumbbell"></i></h2>
-        <div class="row">
-            @foreach ($topGyms as $gym)
-                @include('components.business-card', ['business' => $gym])
-            @endforeach
-        </div>
-    </div> --}}
 
     <div class="container my-5">
         <h2 class="mb-4">Top Rated Professionals</h2>
     </div>
     @include('components.user-search', ['professions' => $professions])
-
-
-    {{-- <div class="container my-5">
-        <h2 class="text-center mb-4">Mechanics <i class="fa-solid fa-gears"></i></h2>
-        <div class="row">
-            @foreach ($topMechanics as $user)
-                @include('components.user-card', ['user' => $user])
-            @endforeach
-        </div>
-    </div> --}}
 
 
     <div class="container my-5">
