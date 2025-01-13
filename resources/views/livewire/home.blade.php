4bbdd0e675d5c3fd398d328a5ebc7de1315fb8ca
waqasra2022skipq <waqas.rahman.sculptor@gmail.com>
2025-01-13 17:18:52 +0500

    Business Card added

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,31 +1,118 @@
 <div>
-    <section class="bg-gray-50 py-20">
-        <div class="container mx-auto text-center">
-            <h1 class="text-4xl font-bold text-gray-800">Discover Top-Rated Professionals and Businesses!</h1>
-            <p class="mt-4 text-gray-600">Explore reviews, ratings, and recommendations to make informed decisions.</p>
-            <div class="mt-6">
-                <input type="text" placeholder="Search businesses..." class="border p-4 rounded-lg w-1/2" />
-                <button class="bg-blue-500 text-white px-6 py-4 rounded-lg ml-2">Search</button>
+    {{-- Hero section Start --}}
+
+    <section class="bg-gray-50 py-20 hero-section">
+        <div class="container mx-auto text-center lg:text-left text-white">
+            <h1 class="text-4xl lg:text-5xl font-bold ">
+                Discover Top-Rated Professionals and Businesses!
+            </h1>
+            <p class="mt-4 text-lg lg:text-xl ">
+                Explore reviews, ratings, and recommendations to make informed decisions.
+            </p>
+            <div class="flex flex-col lg:flex-row  items-start gap-10 mt-10 text-black">
+                <!-- Business Search Form -->
+                <div class="bg-white p-6 rounded-lg shadow-lg w-full lg:w-1/3">
+                    <h2 class="text-xl font-semibold text-gray-800">Search Businesses</h2>
+                    <form class="mt-4 space-y-4" action="{{ route('allBusinesses') }}">
+                        <!-- Category -->
+                        <div>
+                            <label for="category" class="block text-sm font-medium text-gray-700">Select a
+                                Category:</label>
+                            <input list="categories" class="w-full rounded px-4 py-2 border border-gray-300"
+                                id="category" name="category" value="{{ request('category') }}"
+                                placeholder="Search Category..." autocomplete="off">
+                            <datalist id="categories" class="custom-select">
+                                <option value="All Categories"></option>
+                                @foreach ($categories as $category)
+                                    <option value="{{ $category->slug }}">{{ $category->name }}</option>
+                                @endforeach
+                            </datalist>
+                        </div>
+                        <!-- Business Name -->
+                        <div>
+                            <label for="business-name" class="block text-sm font-medium text-gray-700">Business
+                                Name</label>
+                            <input type="text" id="business-name" name="search"
+                                class="block w-full border-gray-300 rounded-lg shadow-sm"
+                                placeholder="Enter business name">
+                        </div>
+                        <!-- Location -->
+                        <div>
+                            <label for="business-location"
+                                class="block text-sm font-medium text-gray-700">Location</label>
+                            <input type="text" id="business-location" name="location"
+                                class="block w-full border-gray-300 rounded-lg shadow-sm" placeholder="Enter location">
+                        </div>
+                        <button
+                            class="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
+                            Search
+                        </button>
+                    </form>
+                </div>
+
+                <!-- Professional Search Form -->
+                <div class="bg-white p-6 rounded-lg shadow-lg w-full lg:w-1/3">
+                    <h2 class="text-xl font-semibold text-gray-800">Search Professionals</h2>
+                    <form class="mt-4 space-y-4" action="{{ route('allUsers') }}">
+                        <!-- Profession -->
+                        <div>
+                            <label for="category" class="block text-sm font-medium text-gray-700">Select a
+                                Profession:</label>
+                            <input list="professions" class="w-full rounded px-4 py-2 border border-gray-300"
+                                id="category" name="profession" value="{{ request('profession') }}"
+                                placeholder="Search profession..." autocomplete="off">
+                            <datalist id="professions" class="custom-select">
+                                <option value="All Categories"></option>
+                                @foreach ($professions as $professions)
+                                    <option value="{{ $professions->slug }}">{{ $professions->name }}</option>
+                                @endforeach
+                            </datalist>
+                        </div>
+                        <!-- Professional Name -->
+                        <div>
+                            <label for="professional-name" class="block text-sm font-medium text-gray-700">Professional
+                                Name</label>
+                            <input type="text" id="professional-name" name="search"
+                                class="block w-full border-gray-300 rounded-lg shadow-sm"
+                                placeholder="Enter professional name">
+                        </div>
+                        <!-- Location -->
+                        <div>
+                            <label for="professional-location"
+                                class="block text-sm font-medium text-gray-700">Location</label>
+                            <input type="text" id="professional-location" name="location"
+                                class="block w-full border-gray-300 rounded-lg shadow-sm" placeholder="Enter location">
+                        </div>
+                        <button
+                            class="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
+                            Search
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     </section>
+
+
+    {{-- Hero section END --}}
     <section class="py-10 bg-white">
         <div class="container mx-auto">
             <h2 class="text-2xl font-bold mb-6">Top Rated Businesses</h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
                 @foreach ($topBusinesses as $business)
                     @livewire('business-card', ['business' => $business])
-                    {{-- <div class="border p-4 rounded-lg shadow">
-                        <h3 class="text-lg font-semibold">{{ $business->name }}</h3>
-                        <p class="text-gray-600">{{ $business->category->name }}</p>
-                        @livewire('review-stars', ['entity' => $business])
-                        <a href="/business/{{ $business->id }}" class="text-blue-500 mt-4 inline-block">View
-                            Details</a>
-                    </div> --}}
                 @endforeach
+            </div>
+            <!-- Browse All Businesses Button -->
+            <div class="mt-8 text-center">
+                <a href="{{ route('allBusinesses') }}"
+                    class="inline-flex items-center px-6 py-3 text-sm font-medium text-white bg-blue-600 rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300">
+                    Browse All Businesses
+                </a>
             </div>
         </div>
     </section>
+
     <section class="py-10 bg-gray-50">
         <div class="container mx-auto">
             <h2 class="text-2xl font-bold mb-6">Browse by Category</h2>
@@ -38,9 +125,28 @@
             </div>
         </div>
     </section>
+
+    {{-- Top Rated Professional --}}
+
+    <section class="py-10 bg-white mb-10">
+        <div class="container mx-auto">
+            <h2 class="text-2xl font-bold mb-6">Top Rated Professional</h2>
+            <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
+                @foreach ($topProfessionals as $professional)
+                    @livewire('professionals.professional-card', ['professional' => $professional])
+                @endforeach
+            </div>
+            <div class="mt-8 text-center">
+                <a href="{{ route('allUsers') }}"
+                    class="inline-flex items-center px-6 py-3 text-sm font-medium text-white bg-blue-600 rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300">
+                    Browse All Professionals
+                </a>
+            </div>
+        </div>
+    </section>
     {{-- Latest Reviews --}}
 
-    <section class="py-10 bg-gray-50">
+    <section class="py-10 bg-green-50">
         <div class="container mx-auto">
             <h2 class="text-2xl font-bold mb-6">Latest Reviews</h2>
             <div
