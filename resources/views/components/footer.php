edabbe89a6d38c6cdcb4a2b1abc89c9d351489f7
waqasra2022skipq <waqas.rahman.sculptor@gmail.com>
2025-01-13 17:17:15 +0500

    footer added

---
+++ resources/views/components/footer.php
@@ -0,0 +1,9 @@
+<footer class="bg-gray-800 text-white py-6">
+    <div class="container mx-auto text-center">
+        <p>&copy; 2025 RateAny. All Rights Reserved.</p>
+        <div class="mt-4 space-x-4">
+            <a href="#" class="text-gray-400 hover:text-white">Privacy Policy</a>
+            <a href="#" class="text-gray-400 hover:text-white">Terms of Service</a>
+        </div>
+    </div>
+</footer>
