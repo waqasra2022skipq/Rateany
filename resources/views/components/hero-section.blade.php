1a00befce649c3b0521de8e926948f68980a18aa
waqasra2022skipq <104453900+waqasra2022skipq@users.noreply.github.com>
2025-01-16 18:55:02 +0500

    Header Section Updated

---
+++ resources/views/components/hero-section.blade.php
@@ -0,0 +1,68 @@
+<div class="mx-auto max-w-7xl sm:px-6 lg:px-8 hero-section text-white">
+    <div class="relative px-6 py-10 overflow-hidden text-center  isolate sm:px-16 sm:shadow-sm dark:bg-transparent">
+
+        <p class="max-w-2xl mx-auto text-3xl font-bold tracking-tight  dark:text-gray-200 sm:text-4xl">
+            Discover Top-Rated Businesses and Professionals!
+        </p>
+
+        <p class="max-w-xl mx-auto mt-6 text-lg leading-8 dark:text-gray-300">
+            Explore reviews, ratings, and recommendations to make informed decisions.
+        </p>
+
+        <form action="{{ route('allBusinesses') }}">
+            <div
+                class="relative flex flex-col items-center justify-center max-w-2xl gap-2 px-2 py-2 mx-auto mt-8 bg-white border shadow-2xl dark:bg-gray-50 min-w-sm md:flex-row rounded-2xl focus-within:border-gray-300">
+
+                <input id="search-bar" placeholder="Business Name" name="name"
+                    class="flex-1 w-full px-6 py-2 bg-white rounded-md outline-none dark:bg-gray-50" required="">
+                <button type="submit"
+                    class="relative w-full px-6 py-3 overflow-hidden text-white transition-all duration-100 bg-black border border-black md:w-auto fill-white active:scale-95 will-change-transform rounded-xl">
+                    <span class="flex items-center transition-all opacity-1">
+                        <span class="mx-auto text-sm font-semibold truncate whitespace-nowrap">
+                            Search
+                        </span>
+                    </span>
+                </button>
+            </div>
+        </form>
+
+        <form action="{{ route('allUsers') }}">
+            <div
+                class="relative flex flex-col items-center justify-center max-w-2xl gap-2 px-2 py-2 mx-auto mt-8 bg-white border shadow-2xl dark:bg-gray-50 min-w-sm md:flex-row rounded-2xl focus-within:border-gray-300">
+
+                <input id="search-bar" placeholder="Professional Name" name="name"
+                    class="flex-1 w-full px-6 py-2 bg-white rounded-md outline-none dark:bg-gray-50" required="">
+                <button type="submit"
+                    class="relative w-full px-6 py-3 overflow-hidden text-white transition-all duration-100 bg-black border border-black md:w-auto fill-white active:scale-95 will-change-transform rounded-xl">
+                    <span class="flex items-center transition-all opacity-1">
+                        <span class="mx-auto text-sm font-semibold truncate whitespace-nowrap">
+                            Search
+                        </span>
+                    </span>
+                </button>
+            </div>
+        </form>
+        <div class="mt-5">
+
+            <a href="{{ route('allBusinesses') }}"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Rate
+                a Business</a>
+            <a href="{{ route('allUsers') }}"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Rate
+                a Professional</a>
+        </div>
+        <svg viewBox="0 0 1024 1024"
+            class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
+            aria-hidden="true">
+            <circle cx="512" cy="512" r="512" fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)"
+                fill-opacity="0.7">
+            </circle>
+            <defs>
+                <radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
+                    <stop stop-color="#3b82f6"></stop>
+                    <stop offset="1" stop-color="#1d4ed8"></stop>
+                </radialGradient>
+            </defs>
+        </svg>
+    </div>
+</div>
