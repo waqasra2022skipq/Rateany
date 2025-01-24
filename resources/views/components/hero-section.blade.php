e215653c25f5a6fbab45ed55f0d07140f4ca9348
waqasra2022skipq <waqas.rahman.sculptor@gmail.com>
2025-01-24 19:14:57 +0500

    Some Fixes for mobile

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -2,7 +2,7 @@
     <div
         class="relative px-6 py-10 overflow-hidden text-center  isolate sm:px-16 sm:shadow-sm dark:bg-transparent bg-[#70696907]">
 
-        <div class="flex flex-col justify-between mt-20">
+        <div class="flex flex-col justify-between md:mt-20">
             <p class="max-w-2xl mx-auto text-3xl font-bold tracking-tight  dark:text-gray-200 sm:text-4xl">
                 Discover Top-Rated Businesses and Professionals!
             </p>
