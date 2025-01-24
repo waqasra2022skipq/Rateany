0525d2f3d92d9033484dae294ac770eb22adfba6
waqasra2022skipq <waqas.rahman.sculptor@gmail.com>
2025-01-24 19:05:47 +0500

    Add app.css to vite config as well FROM CSS folder

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 "resources/sass/app.scss",
                 "resources/js/app.js",
-                "resources/sass/app.css",
+                "resources/css/app.css",
             ],
             refresh: true,
         }),
