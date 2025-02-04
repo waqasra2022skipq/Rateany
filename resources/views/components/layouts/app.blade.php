8ef8f873d2014a64022964a3de78a6085ad5f9e7
waqasra2022skipq <waqas.rahman.sculptor@gmail.com>
2025-02-04 18:17:11 +0500

    Loading favicon dynamically

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,9 @@
 
     <head>
         <meta charset="utf-8">
-        <link rel="icon" href="images/OIG2.x5WJPvCi944TyE_040uC.jpg" />
+        {{-- <link rel="icon" href="images/OIG2.x5WJPvCi944TyE_040uC.jpg" /> --}}
+        <link rel="icon" href="{{ asset('images/OIG2.x5WJPvCi944TyE_040uC.jpg') }}" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
         <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
