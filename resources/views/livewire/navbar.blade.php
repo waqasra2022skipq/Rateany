abcec0641eb2f125ae49f69cc011a64ddc93b48d
waqasra2022skipq <waqas.rahman.sculptor@gmail.com>
2025-02-04 14:55:18 +0500

    Navbar updated

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -49,28 +49,6 @@
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
             <ul
                 class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                @guest
-                    <li>
-                        <a href="{{ route('login') }}"
-                            class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent {{ $this->isActive('login') }}">
-                            Login
-
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('register') }}"
-                            class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent {{ $this->isActive('register') }}">
-                            Register
-
-                        </a>
-                    </li>
-                @endguest
-                <li>
-                    <a class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent {{ $this->isActive('register') }}"
-                        href="{{ route('contact.show') }}">
-                        Contact Us
-                    </a>
-                </li>
                 <li>
                     <a class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent {{ $this->isActive('register') }}"
                         href="{{ route('allBusinesses') }}">
@@ -81,6 +59,22 @@
                         href="{{ route('allUsers') }}" class="text-white mx-3 user-link">
                         Professionals</a>
                 </li>
+                <li>
+                    <a class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent {{ $this->isActive('register') }}"
+                        href="{{ route('contact.show') }}">
+                        Contact Us
+                    </a>
+                </li>
+                @guest
+                    <li>
+                        <a href="{{ route('login') }}"
+                            class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent {{ $this->isActive('login') }}">
+                            Login
+
+                        </a>
+                    </li>
+
+                @endguest
 
             </ul>
         </div>
