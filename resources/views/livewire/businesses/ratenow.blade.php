11a7186363ec3aede2e152fa7d45e093cc96c948
waqasra2022skipq <waqas.rahman.sculptor@gmail.com>
2025-03-16 12:38:18 +0500

    Placeholders added

--- resources/views/livewire/businesses/ratenow.blade.php
+++ resources/views/livewire/businesses/ratenow.blade.php
@@ -2,35 +2,39 @@
     <!-- Review Form -->
     <form wire:submit.prevent="submitReview">
         <!-- Business Information (if not found) -->
-        <div class="mb-6">
-            <label class="block text-sm font-medium text-gray-700 mb-2">Name <b class="text-red-500">*</b></label>
-            <input wire:model="business_name" type="text"
-                class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
-            @error('business_name')
-                <span class="text-sm text-red-600">{{ $message }}</span>
-            @enderror
-        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+            <div class="mb-6">
+                <label class="block text-sm font-medium text-gray-700 mb-2">Name <b class="text-red-500">*</b></label>
+                <input wire:model="business_name" type="text"
+                    placeholder="Enter the name of business,product or professional"
+                    class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
+                @error('business_name')
+                    <span class="text-sm text-red-600">{{ $message }}</span>
+                @enderror
+            </div>
 
-        <div class="mb-6">
-            <label class="block text-sm font-medium text-gray-700 mb-2">Category <b class="text-red-500">*</b></label>
+            <div class="mb-6">
+                <label class="block text-sm font-medium text-gray-700 mb-2">Category <b
+                        class="text-red-500">*</b></label>
 
-            <!-- Search by Category -->
-            <input list="categories" wire:model="business_category" placeholder="Select Category"
-                class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300">
-            <datalist id="categories" class="">
-                <option value="All Categories"></option>
-                @foreach ($categories as $category)
-                    <option value="{{ $category->name }}">{{ $category->name }}</option>
-                @endforeach
-            </datalist>
-            @error('business_category')
-                <span class="text-sm text-red-600">{{ $message }}</span>
-            @enderror
+                <!-- Search by Category -->
+                <input list="categories" wire:model="business_category" placeholder="Select Category"
+                    class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300">
+                <datalist id="categories" class="">
+                    <option value="All Categories"></option>
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->name }}">{{ $category->name }}</option>
+                    @endforeach
+                </datalist>
+                @error('business_category')
+                    <span class="text-sm text-red-600">{{ $message }}</span>
+                @enderror
+            </div>
         </div>
 
         <div class="mb-6">
             <label class="block text-sm font-medium text-gray-700 mb-2">Address</label>
-            <input wire:model="business_location" type="text"
+            <input wire:model="business_location" type="text" placeholder="Enter the address of the business"
                 class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
             @error('business_location')
                 <span class="text-sm text-red-600">{{ $message }}</span>
@@ -39,7 +43,7 @@
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
             <div class="mb-6">
                 <label class="block text-sm font-medium text-gray-700 mb-2">Phone</label>
-                <input wire:model="contact_phone" type="text"
+                <input wire:model="contact_phone" type="text" placeholder="Enter the contact number of the business"
                     class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
                 @error('contact_phone')
                     <span class="text-sm text-red-600">{{ $message }}</span>
@@ -47,9 +51,10 @@
             </div>
             <div class="mb-6">
                 <label class="block text-sm font-medium text-gray-700 mb-2">Website</label>
-                <input wire:model="contact_phone" type="text"
+                <input wire:model="contact_website" type="text"
+                    placeholder="Enter the website address of the business"
                     class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
-                @error('contact_phone')
+                @error('contact_website')
                     <span class="text-sm text-red-600">{{ $message }}</span>
                 @enderror
             </div>
@@ -93,7 +98,7 @@
                 <div>
                     <label for="reviewer_name" class="block text-sm font-medium text-gray-700 mb-2">Your
                         Name <b class="text-red-500">*</b></label>
-                    <input wire:model="reviewer_name" type="text" id="reviewer_name"
+                    <input wire:model="reviewer_name" type="text" id="reviewer_name" placeholder="Enter Your Name"
                         class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-600 focus:border-primary-600">
                     @error('reviewer_name')
                         <span class="text-sm text-red-600">{{ $message }}</span>
@@ -104,7 +109,7 @@
                 <div>
                     <label for="reviewer_email" class="block text-sm font-medium text-gray-700 mb-2">Your
                         Email <b class="text-red-500">*</b></label>
-                    <input wire:model="reviewer_email" type="email" id="reviewer_email"
+                    <input wire:model="reviewer_email" type="email" id="reviewer_email" placeholder="Enter your Email"
                         class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-600 focus:border-primary-600">
                     @error('reviewer_email')
                         <span class="text-sm text-red-600">{{ $message }}</span>
