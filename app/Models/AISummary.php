4e135f62c5484bb5e63f5f94c28384ccc6926a69
Waqas Rahman <waqasrehman@invotyx.co.uk>
2025-05-05 17:31:11 +0500

    AISummary model added

---
+++ app/Models/AISummary.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AISummary extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'business_id',
+        'ai_summary',
+        'created_at',
+        'updated_at',
+    ];
+
+    public $table = 'ai_summaries';
+}
